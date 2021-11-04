383885b7a1fece8adccedc1663715563e8a283b5
Deepak <deepakpatil0261@gmail.com>
2021-11-04 19:09:58 +0530

    Second commit

--- src/Default/Test1.java
+++ src/Default/Test1.java
@@ -5,6 +5,8 @@ public class Test1 {
 	public static void main(String[] args) {
 
 		String hungry = "yes";
+		
+		System.out.println("This IS My First Git Repository Program Uploaded On Github ");
 
 		if (hungry == "yes") {
 			System.out.println("Eat Something.........!!");
